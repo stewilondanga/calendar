14ef1f2afbe53be69d7fb7b84c1870873f37a736
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 08:18:48 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
-var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
-})();
+var months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var colors = ["#16a085", "#1abc9c", "#c0392b", "#27ae60", "#FF6860", "#f39c12", "#f1c40f", "#e67e22", "#2ecc71", "#e74c3c", "#d35400", "#2c3e50"];
 
-<script>
-	window.onload = function() {
-		new Calendar('calendar');
-	};
-</script>
+function Calendar(elemId) {
+    this.elmDomObj = document.getElementById(elemId);
+    this.drawCalendar();
+    this.initCalendar();
+    delete this.elmDomObj;
+}
