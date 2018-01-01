6ffd54eb46788b63a38be9a032a908d11fcfc98c
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 08:25:38 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -108,3 +108,95 @@ return (new Date(year, month, 0)).getDate()
             }
         }
     },
+		_verifyIfToday: function (e) {
+			 return this._getFormattedDate(new Date) == this._getFormattedDate(e)
+	 },
+	 _getFormattedDate: function (e) {
+			 return e.getFullYear() + "/" + (e.getMonth()) + "/" + e.getDate()
+	 },
+	 _highlightToday: function () {
+			 var _that = this
+			 var y = colors[this.currMonth];
+			 this.header.style.height = `${this.hw}px`;
+			 this.header.style.backgroundColor = y;
+			 let h1s = document.querySelectorAll("div#calendar_header h1");
+			 h1s.forEach(function (e) {
+					 e.style.backgroundColor = y;
+					 e.innerHTML = months[_that.currMonth] + " " + _that.currYear;
+			 });
+			 let wDays = document.querySelectorAll("div#calendar_weekdays div");
+			 let today = document.querySelectorAll("div#calendar_content .today");
+			 wDays.forEach(function (e) {
+					 e.style.color = y;
+			 });
+			 today.forEach(function (e) {
+					 e.style.backgroundColor = y;
+			 });
+			 let icons = document.querySelectorAll("div#calendar_header i[class^='icon-chevron'");
+			 icons.forEach(function (e) {
+					 e.style.lineHeight = `${_that.hw}px`;
+			 });
+			this._highlightDate();
+	 },
+	 _navigate: function (e) {
+			 e.stopPropagation();
+			 if (e.target.getAttribute("class").indexOf("left") != -1) {
+					 return this._navigateMonths("previous");
+			 } else {
+					 return this._navigateMonths("next");
+			 }
+	 },
+		_navigateKeys: function (e) {
+		 if (e.keyCode == '37') {
+			// left arrow
+			 return this._navigateMonths("previous");
+	 }
+	 else if (e.keyCode == '39') {
+			// right arrow
+			 return this._navigateMonths("next");
+	 }},
+	 _navigateMonths: function (e) {
+			 this.currMonth = e == "next" ? this.currMonth + 1 : this.currMonth - 1;
+			 if (this.currMonth < 0) {
+					 this.currMonth = 11;
+					 this.currYear--;
+			 } else if (this.currMonth > 11) {
+					 this.currMonth = 0;
+					 this.currYear++;
+			 }
+			 this._generate();
+			 return;
+	 },
+	 _selectDate: function (e) {
+			 e.stopPropagation();
+			 var y = colors[_that.currMonth];
+			 this.currDate = e.target.innerText;
+			 this.currDay = this.currDate;
+			 this._highlightDate();
+			 let ldays = document.querySelectorAll("div#calendar_content div:not(.blank)");
+
+				ldays.forEach(function (f){
+					 f.style.backgroundColor = '#ffffff';
+					 f.style.color= '#787878';
+					 f.style.opacity= '1';
+				});
+				 e.target.style.backgroundColor = y;
+				 e.target.style.opacity = '0.7';
+				 e.target.style.color = '#FFFFFF';
+	 },
+	 _highlightDate: function(){
+			 _that = this;
+			 var y = colors[_that.currMonth];
+				let ldays = document.querySelectorAll("div#calendar_left .c_day");
+			 ldays.forEach(function (e){
+					 e.innerHTML = _that.currMonthDates[_that.currDay-1].weekday;
+					 e.style.backgroundColor = y;
+					 e.style.height = `${_that.hw}px`;
+					 e.style.lineHeight = `${_that.hw}px`;
+			 });
+			 let dates = document.querySelectorAll("div#calendar_left .c_date");
+			 dates.forEach(function (e){
+					 e.innerHTML = _that.currDate;
+			 });
+	 }
+}
